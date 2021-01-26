77960105d7ff5084a5c82d19b2d05ec46e6d67ff
Илья Никулин <i.nikulin@sfbtech.org>
2021-01-26 18:50:40 +0300

    star: make transform functions in swapi-service

--- star-db/src/components/person-details/person-details.js
+++ star-db/src/components/person-details/person-details.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import './person-details.css';
 
 export default class PersonDetails extends Component {
-
+  
   render() {
     return (
       <div className="person-details card">
         <img className="person-image"
-             src="https://starwars-visualguide.com/assets/img/characters/3.jpg" />
-
+             src="https://starwars-visualguide.com/assets/img/characters/3.jpg"
+             alt="person"/>
+        
         <div className="card-body">
           <h4>R2-D2</h4>
           <ul className="list-group list-group-flush">
