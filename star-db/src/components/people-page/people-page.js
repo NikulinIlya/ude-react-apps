381e1e49403504593c841b6832e1d8023ba31409
Илья Никулин <i.nikulin@sfbtech.org>
2021-02-01 14:19:01 +0300

    refactor(star/people-page): make row component

--- star-db/src/components/people-page/people-page.js
+++ star-db/src/components/people-page/people-page.js
@@ -6,19 +6,7 @@ import ErrorIndicator from '../error-indicator/error-indicator';
 
 import './people-page.css';
 import SwapiService from "../../services/swapi-service";
-
-const Row = ({left, right}) => {
-  return (
-    <div className="row mb2">
-      <div className="col-md-6">
-        {left}
-      </div>
-      <div className="col-md-6">
-        {right}
-      </div>
-    </div>
-  );
-};
+import Row from "../row";
 
 export default class PeoplePage extends Component {
   swapiService = new SwapiService();
