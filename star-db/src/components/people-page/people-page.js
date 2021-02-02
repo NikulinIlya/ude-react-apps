710d115632c0f363ad6d18e413ebc505788bac33
Илья Никулин <i.nikulin@sfbtech.org>
2021-02-02 18:23:20 +0300

    ref(star): make hoc-helpers/with-data.js

--- star-db/src/components/people-page/people-page.js
+++ star-db/src/components/people-page/people-page.js
@@ -1,31 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import ItemList from '../item-list/item-list';
-import PersonDetails from '../person-details/person-details';
-import ErrorIndicator from '../error-indicator/error-indicator';
+import ItemDetails from '../item-details/item-details';
+import SwapiService from '../../services/swapi-service';
+import Row from '../row';
+import ErrorBoundry from '../error-boundry';
 
 import './people-page.css';
-import SwapiService from "../../services/swapi-service";
-import Row from "../row";
-import ErrorBoundry from '../error-boundry';
 
 export default class PeoplePage extends Component {
   swapiService = new SwapiService();
   
   state = {
-    selectedPerson: 3
+    selectedPerson: 11
   };
   
   onPersonSelected = (selectedPerson) => {
-    this.setState({selectedPerson});
+    this.setState({ selectedPerson });
   };
   
   render() {
-    
-    if (this.state.hasError) {
-      return <ErrorIndicator/>;
-    }
-    
     const itemList = (
       <ItemList
         onItemSelected={this.onPersonSelected}
@@ -34,17 +28,18 @@ export default class PeoplePage extends Component {
         {(i) => (
           `${i.name} (${i.birthYear})`
         )}
+      
       </ItemList>
     );
     
     const personDetails = (
       <ErrorBoundry>
-        <PersonDetails personId={this.state.selectedPerson}/>
+        <ItemDetails itemId={this.state.selectedPerson} />
       </ErrorBoundry>
     );
     
     return (
-      <Row left={itemList} right={personDetails}/>
+      <Row left={itemList} right={personDetails} />
     );
   }
 }
