fb26416ff047f8471e7a5807d82fc6c903074019
Илья Никулин <i.nikulin@sfbtech.org>
2021-02-04 18:25:29 +0300

    ref(star): use composition of hocs

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -77,16 +77,11 @@ export default class App extends Component {
           <PlanetDetails itemId={5}/>
           <StarshipDetails itemId={9}/>
           
-          <PersonList>
-            { ({name}) => <span>{name}</span> }
-          </PersonList>
-          <StarshipList>
-            { ({name}) => <span>{name}</span> }
-          </StarshipList>
-          <PlanetList>
-            { ({name}) => <span>{name}</span> }
-          </PlanetList>
-        
+          <PersonList />
+
+          <StarshipList />
+
+          <PlanetList />
         </div>
       </ErrorBoundry>
     );
