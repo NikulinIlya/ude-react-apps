004e3fe8db7f7779cdd726ef81cfc133eb313e03
Илья Никулин <i.nikulin@sfbtech.org>
2021-02-19 15:13:34 +0300

    ref(star): change item-lists.js

--- star-db/src/components/sw-components/item-lists.js
+++ star-db/src/components/sw-components/item-lists.js
@@ -15,27 +15,41 @@ const withChildFunction = (Wrapped, fn) => {
 const renderName = ({name}) => <span>{name}</span>
 const renderModelAndName = ({model, name}) => <span>{name} ({model})</span>;
 
+const mapPersonMethodsToProps = (swapiService) => {
+  return {
+    getData: swapiService.getAllPeople
+  };
+};
+
+const mapPlanetMethodsToProps = (swapiService) => {
+  return {
+    getData: swapiService.getAllPlanets
+  };
+};
+
+const mapStarshipMethodsToProps = (swapiService) => {
+  return {
+    getData: swapiService.getAllStarships
+  };
+};
+
 const PersonList = withSwapiService(withData(
   withChildFunction(
     ItemList,
     renderName
-  )));
+  )), mapPersonMethodsToProps);
 
-const mapPersonMethodsToProps = () => {
-
-}
 const PlanetList = withSwapiService(withData(
   withChildFunction(
     ItemList,
     renderName
-  )),
-  mapPersonMethodsToProps);
+  )), mapPlanetMethodsToProps);
 
 const StarshipList = withSwapiService(withData(
   withChildFunction(
     ItemList,
     renderModelAndName
-  )));
+  )), mapStarshipMethodsToProps);
 
 export {
   PersonList,
