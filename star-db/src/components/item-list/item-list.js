064a770971874959ee7cf528eebfd0d9062ee0ca
Илья Никулин <i.nikulin@sfbtech.org>
2021-03-03 18:28:56 +0300

    feat(star): install and use prop-types lib

--- star-db/src/components/item-list/item-list.js
+++ star-db/src/components/item-list/item-list.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './item-list.css';
 
@@ -29,4 +30,9 @@ ItemList.defaultProps = {
   onItemSelected: () => {}
 };
 
+ItemList.propTypes = {
+  onItemSelected: PropTypes.func,
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+  children: PropTypes.func.isRequired
+}
 export default ItemList;
