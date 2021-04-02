7d0a945f15b75b622b9e73c93db08f77a406883f
NikulinIlya <ilia.nikulin@gmail.com>
2021-04-02 17:19:58 +0300

    hooks: useEffect 2

--- hooks/src/use-effect.js
+++ hooks/src/use-effect.js
@@ -10,7 +10,7 @@ const App = () => {
       <div>
         <button onClick={() => setValue((v) => v + 1)}>+</button>
         <button onClick={() => setVisible(false)}>hide</button>
-        <HookCounter value={value}/>
+        <Notification/>
       </div>
     );
   } else {
@@ -27,6 +27,20 @@ const HookCounter = ({value}) => {
   useEffect(() => console.log('update'));
 
   return <p>{value}</p>
+}
+
+const Notification = () => {
+  const [visible, setVisible] = useState(true);
+  useEffect(() => {
+    const timeout = setTimeout(() => setVisible(false), 2500);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <div>
+      {visible && <p>Hello</p>}
+    </div>
+  );
 }
 
 ReactDOM.render(<App/>,
