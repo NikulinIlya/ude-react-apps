e1c1db78afb9f0260ec7c7453c0b19c68109604f
NikulinIlya <ilia.nikulin@gmail.com>
2021-04-02 17:10:12 +0300

    hooks: useEffect 1

---
+++ hooks/src/use-effect.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  const [value, setValue] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  if (visible) {
+    return (
+      <div>
+        <button onClick={() => setValue((v) => v + 1)}>+</button>
+        <button onClick={() => setVisible(false)}>hide</button>
+        <HookCounter value={value}/>
+      </div>
+    );
+  } else {
+    return <button onClick={() => setVisible(true)}>show</button>;
+  }
+};
+
+const HookCounter = ({value}) => {
+  useEffect(() => {
+    console.log('mount');
+    return () => console.log('unmount')
+  }, []);
+
+  useEffect(() => console.log('update'));
+
+  return <p>{value}</p>
+}
+
+ReactDOM.render(<App/>,
+  document.getElementById('root'));
